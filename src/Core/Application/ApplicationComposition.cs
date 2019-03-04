3645ae7e9315fc67e0c72fdc6a879b4f20f5618c
Scott Ferguson <scott.ferguson82@gmail.com>
2019-03-04 17:22:54 -0600

    Adding ApplicationComposition

---
+++ src/Core/Application/ApplicationComposition.cs
@@ -0,0 +1,10 @@
+﻿using Core.Framework;
+using System.Collections.Generic;
+
+namespace Core.Application
+{
+    public class ApplicationComposition
+    {
+        public List<IPlugin> IPlugins { get; set; }
+    }
+}
