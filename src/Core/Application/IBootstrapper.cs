629a67b8c94ce00b94e009aab1712284c639bb6f
Scott Ferguson <scott.ferguson82@gmail.com>
2019-03-05 12:22:25 -0600

    Adding a generic type for IBootstrapper

--- src/Core/Application/IBootstrapper.cs
+++ src/Core/Application/IBootstrapper.cs
@@ -3,6 +3,17 @@
     /// <summary>
     /// A generic abstraction of an application bootstrapper
     /// </summary>
+    public interface IBootstrapper<TInput, TOutput>
+    {
+        /// <summary>
+        /// Called by the Core application framework during application startup
+        /// </summary>
+        TOutput Startup(TInput input);
+    }
+
+    /// <summary>
+    /// A generic abstraction of an application bootstrapper
+    /// </summary>
     public interface IBootstrapper
     {
         /// <summary>
