8ff7e8e2ab81a00185c47e94f0ef128bb7617f24
Scott Ferguson <scott.ferguson82@gmail.com>
2019-03-25 16:57:50 -0500

    Rolling back a break in a Bootstrapper consumer

--- src/Core/Application/IBootstrapper.cs
+++ src/Core/Application/IBootstrapper.cs
@@ -3,22 +3,22 @@
     /// <summary>
     /// A generic abstraction of an application bootstrapper
     /// </summary>
-    public interface IBootstrapper
-    {
-        /// <summary>
-        /// Called by the Core application framework during application startup
-        /// </summary>
-        StartupResult Startup();
-    }
-
-    /// <summary>
-    /// A generic abstraction of an application bootstrapper
-    /// </summary>
     public interface IBootstrapper<TInput, TOutput>
     {
         /// <summary>
         /// Called by the Core application framework during application startup
         /// </summary>
-        StartupResult<TOutput> Startup(TInput input);
+        TOutput Startup(TInput input);
+    }
+
+    /// <summary>
+    /// A generic abstraction of an application bootstrapper
+    /// </summary>
+    public interface IBootstrapper
+    {
+        /// <summary>
+        /// Called by the Core application framework during application startup
+        /// </summary>
+        void Startup();
     }
 }
