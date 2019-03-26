804c492d96e490377a6d6af517f186c0fde2308d
Scott Ferguson <scott.ferguson82@gmail.com>
2019-03-25 21:06:02 -0500

    Adding Crud and Lookup services

---
+++ src/Core/Framework/Services/ICrudService.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.Framework.Services
+{
+    /// <summary>
+    /// A generic abstraction of a service that contains all basic CRUD methods
+    /// </summary>
+    /// <typeparam name="TItem">The type of item being operated on</typeparam>
+    public interface ICrudService<TItem> : IService
+    {
+        /// <summary>
+        /// Creates the item
+        /// </summary>
+        /// <param name="item">The item to create</param>
+        /// <returns>The ID of the item created</returns>
+        long Create(TItem item);
+
+        /// <summary>
+        /// Creates the item asyncronously
+        /// </summary>
+        /// <param name="item">The item to create</param>
+        /// <returns>The ID of the item created</returns>
+        Task<long> CreateAsync(TItem item);
+
+        /// <summary>
+        /// Gets the item by ID
+        /// </summary>
+        /// <param name="id">The ID of the item to return</param>
+        /// <returns>The item if found; otherwise, null</returns>
+        TItem GetByID(long id);
+
+        /// <summary>
+        /// Gets the item by ID asyncronously
+        /// </summary>
+        /// <param name="id">The ID of the item to return</param>
+        /// <returns>The item if found; otherwise, null</returns>
+        Task<TItem> GetByIDAsync(long id);
+
+        /// <summary>
+        /// Updates the item
+        /// </summary>
+        /// <param name="item">The item to update</param>
+        void Update(TItem item);
+
+        /// <summary>
+        /// Updates the item asyncronously
+        /// </summary>
+        /// <param name="item">The item to update</param>
+        Task UpdateAsync(TItem item);
+
+        /// <summary>
+        /// Deletes the item
+        /// </summary>
+        /// <param name="id">The ID of the item to delete</param>
+        void Delete(long id);
+
+        /// <summary>
+        /// Deletes the item asyncronously
+        /// </summary>
+        /// <param name="id">The ID of the item to delete</param>
+        Task DeleteAsync(long id);
+    }
+}
---
+++ src/Core/Framework/Services/ILookupService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Framework.Services
+{
+    /// <summary>
+    /// A generic abstraction of a service that retuns static lookup data
+    /// </summary>
+    /// <typeparam name="TItem">The type of item to be returned</typeparam>
+    public interface ILookupService<TItem> : IService
+    {
+        /// <summary>
+        /// Returns all lookup data items
+        /// </summary>
+        /// <returns>A collection of all items</returns>
+        IEnumerable<TItem> GetAll();
+
+        /// <summary>
+        /// Returns all lookup data items asyncronously
+        /// </summary>
+        /// <returns>A collection of all items</returns>
+        Task<IEnumerable<TItem>> GetAllAsync();
+    }
+}
