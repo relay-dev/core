be8d012bdf7e2b948683410a676c32290b357b78
Scott Ferguson <scott.ferguson82@gmail.com>
2019-03-29 22:39:40 -0500

    Adding CacheEntry

---
+++ src/Core/Caching/CacheEntry.cs
@@ -0,0 +1,23 @@
+﻿namespace Core.Caching
+{
+    /// <summary>
+    /// A generic representation of an entry in a cache
+    /// </summary>
+    /// <typeparam name="TToCache">The type of object to cache</typeparam>
+    public class CacheEntry<TToCache>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="value"></param>
+        public CacheEntry(TToCache value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// The value of the cache entry
+        /// </summary>
+        public TToCache Value { get; }
+    }
+}
