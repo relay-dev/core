2b6ef47e6fbced7fcc243bfc01dd1863e70050e3
Scott Ferguson <scott.ferguson82@gmail.com>
2019-03-31 21:50:19 -0500

    Altering the IApplicationContextProvider as it looks like it suffered from being defined as a class

--- src/Core/Providers/IApplicationContextProvider.cs
+++ src/Core/Providers/IApplicationContextProvider.cs
@@ -6,26 +6,5 @@ namespace Core.Providers
     /// <summary>
     /// Provides consumers with the current ApplicationContext
     /// </summary>
-    public class IApplicationContextProvider : IProvider<ApplicationContext>
-    {
-        private readonly ApplicationContext _applicationContext;
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="applicationContext">The current ApplicationContext</param>
-        public IApplicationContextProvider(ApplicationContext applicationContext)
-        {
-            _applicationContext = applicationContext;
-        }
-
-        /// <summary>
-        /// Gets the current ApplicationContext
-        /// </summary>
-        /// <returns>The current ApplicationContext</returns>
-        public ApplicationContext Get()
-        {
-            return _applicationContext;
-        }
-    }
+    public interface IApplicationContextProvider : IProvider<ApplicationContext> { }
 }
