ff69dbc980d380962848a3fc2506cfc42073d6fc
Scott Ferguson <scott.ferguson82@gmail.com>
2019-04-04 15:20:15 -0500

    Adding IKeyProvider

---
+++ src/Core/Providers/IKeyProvider.cs
@@ -0,0 +1,17 @@
+﻿using Core.Framework;
+
+namespace Core.Providers
+{
+    /// <summary>
+    /// Provides Commands with keys, sych as encryption keys
+    /// </summary>
+    public interface IKeyProvider : IProvider<string>
+    {
+        /// <summary>
+        /// Returns the key associated to the keyIdentifier
+        /// </summary>
+        /// <param name="keyIdentifier">A unique identifier of the key to return</param>
+        /// <returns></returns>
+        string Get(string keyIdentifier);
+    }
+}
