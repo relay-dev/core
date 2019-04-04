7f51362c7326255d382c4a58ea74abab9905d52b
Scott Ferguson <scott.ferguson82@gmail.com>
2019-04-04 18:05:36 -0500

    Altering the signatures on IAssemblyScanner slightly

--- src/Core/Utilities/IAssemblyScanner.cs
+++ src/Core/Utilities/IAssemblyScanner.cs
@@ -16,24 +16,11 @@ namespace Core.Utilities
         List<Assembly> GetApplicationAssemblies();
 
         /// <summary>
-        /// Gets all Core assemblies within the scope of the application
-        /// </summary>
-        /// <returns>All Core assemblies within the scope of the application</returns>
-        List<Assembly> GetCoreAssemblies();
-
-        /// <summary>
-        /// Scans all Core assemblies and returns a collection of types that utilize a given attribute type
-        /// </summary>
-        /// <typeparam name="TAttribute">The attribute type to search all class in the assemblies for</typeparam>
-        /// <returns>All Core types that utilize the given attribute type</returns>
-        List<Type> GetCoreTypesWithAttribute<TAttribute>() where TAttribute : Attribute;
-
-        /// <summary>
         /// Scans all Core assemblies and returns a collection of types that utilize a given attribute type and match the pedicate argument
         /// </summary>
         /// <typeparam name="TAttribute">The attribute type to search all class in the assemblies for</typeparam>
-        /// <param name="predicate">The condition of the types to match in order to include them in the return collection</param>
+        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
         /// <returns>All Core types that utilize the given attribute type and match the pedicate argument</returns>
-        List<Type> GetCoreTypesWithAttribute<TAttribute>(Func<TAttribute, bool> predicate) where TAttribute : Attribute;
+        List<Type> GetApplicationTypesWithAttribute<TAttribute>(Func<TAttribute, bool> predicate = null) where TAttribute : Attribute;
     }
 }
