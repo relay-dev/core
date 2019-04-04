9d68205491ac0668fbcf6889d548b1ca4de7ffea
Scott Ferguson <scott.ferguson82@gmail.com>
2019-04-04 18:16:53 -0500

    Adding back IAssemblyScanner.GetApplicationTypesWithAttribute()

--- src/Core/Utilities/IAssemblyScanner.cs
+++ src/Core/Utilities/IAssemblyScanner.cs
@@ -19,6 +19,13 @@ namespace Core.Utilities
         /// Scans all Core assemblies and returns a collection of types that utilize a given attribute type and match the pedicate argument
         /// </summary>
         /// <typeparam name="TAttribute">The attribute type to search all class in the assemblies for</typeparam>
+        /// <returns>All Core types that utilize the given attribute type and match the pedicate argument</returns>
+        List<Type> GetApplicationTypesWithAttribute<TAttribute>() where TAttribute : Attribute;
+
+        /// <summary>
+        /// Scans all Core assemblies and returns a collection of types that utilize a given attribute type and match the pedicate argument
+        /// </summary>
+        /// <typeparam name="TAttribute">The attribute type to search all class in the assemblies for</typeparam>
         /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
         /// <returns>All Core types that utilize the given attribute type and match the pedicate argument</returns>
         List<Type> GetApplicationTypesWithAttribute<TAttribute>(Func<TAttribute, bool> predicate = null) where TAttribute : Attribute;
