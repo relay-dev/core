710f64209bca9a2db7dd75c7f0ce583a91b66146
Scott Ferguson <scott.ferguson82@gmail.com>
2019-12-19 21:03:43 -0600

    Core should really house the Processor classes. Moving them from the Plugins assemblies

---
+++ src/Core/Application/IProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Application
+{
+    /// <summary>
+    /// Represents a simple class that runs a process
+    /// </summary>
+    public interface IProcessor
+    {
+        /// <summary>
+        /// Invokes the process
+        /// </summary>
+        void Process();
+    }
+}
---
+++ src/Core/Application/ProcessorAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core.Application
+{
+    /// <summary>
+    /// Represents a simple class that runs a process
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
+    public sealed class ProcessorAttribute : Attribute
+    {
+        /// <summary>
+        /// Specifies the name of the process the class represents
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
