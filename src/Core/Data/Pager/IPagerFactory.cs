a47ccca0793e126df28d3f3728e93644e74a61fa
Scott Ferguson <scott.ferguson82@gmail.com>
2020-02-03 11:32:34 -0600

    Adding Pager abstractions

---
+++ src/Core/Data/Pager/IPagerFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Data.Pager
+{
+    /// <summary>
+    /// Creates an <see cref="IPager"/> instance for the pagination of views
+    /// </summary>
+    public interface IPagerFactory
+    {
+        /// <summary>
+        /// Creates an instance of an IPager
+        /// </summary>
+        /// <param name="connectionName">The name of the connection string in your configuration which the Pager use to connect to the resource</param>
+        /// <returns>A new instance of an IPager with the proper connection string</returns>
+        IPager Create(string connectionName);
+    }
+}
---
+++ src/Core/Data/Pager/PagerResult.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+
+namespace Core.Data.Pager
+{
+    /// <summary>
+    /// The result of a pagination command
+    /// </summary>
+    public class PagerResult
+    {
+        /// <summary>
+        /// Created a new instance of a PagerResult
+        /// </summary>
+        /// <param name="dataTable">The dataTable to be accessible as the result of the pagination</param>
+        /// <param name="totalCount">The total count of all records in the view</param>
+        public PagerResult(DataTable dataTable, int totalCount)
+        {
+            DataTable = dataTable;
+            TotalCount = totalCount;
+        }
+
+        /// <summary>
+        /// The count of all records returned
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return DataTable.Rows.Count;
+            }
+        }
+
+        /// <summary>
+        /// The total count of all records in the view
+        /// </summary>
+        public int TotalCount { get; }
+
+        /// <summary>
+        /// The records returned for this iteration of the pager
+        /// </summary>
+        public DataTable DataTable { get; }
+    }
+}
