d076e554cc9c8845487e3956fca139f81180cd19
Scott Ferguson <scott.ferguson82@gmail.com>
2020-02-17 13:17:23 -0600

    Adding some FTP utilities

---
+++ src/Core/Files/IFtpClient.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Core.Files
+{
+    /// <summary>
+    /// A generic abstraction of an FTP client
+    /// </summary>
+    public interface IFtpClient
+    {
+        /// <summary>
+        /// Deletes the remote file
+        /// </summary>
+        /// <param name="filePath">The remote file path, including the file name</param>
+        void DeleteFile(string filePath);
+
+        /// <summary>
+        /// Downloads a remote file to a stream
+        /// </summary>
+        /// <param name="filePath">The remote file path, including the file name</param>
+        /// <returns>A stream of the file</returns>
+        Stream DownloadFile(string filePath);
+
+        /// <summary>
+        /// Gets the DateTimestamp of the remote file
+        /// </summary>
+        /// <param name="filePath">The remote file path, including the file name</param>
+        /// <returns>The DateTimestamp</returns>
+        string GetDateTimestamp(string filePath);
+
+        /// <summary>
+        /// Gets the file size of the remote file
+        /// </summary>
+        /// <param name="filePath">The remote file path, including the file name</param>
+        /// <returns>The file size</returns>
+        string GetFileSize(string filePath);
+
+        /// <summary>
+        /// Lists all files in a remote directory
+        /// </summary>
+        /// <param name="directoryPath">The remote directory path</param>
+        /// <returns>A list of all files in the remote directory</returns>
+        List<string> ListDirectory(string directoryPath);
+
+        /// <summary>
+        /// Lists all files in a remote directory with extended details
+        /// </summary>
+        /// <param name="directoryPath">The remote directory path</param>
+        /// <returns>A list of all files in the remote directory</returns>
+        List<string> ListDirectoryDetails(string directoryPath);
+
+        /// <summary>
+        /// Makes a directory on the remote target
+        /// </summary>
+        /// <param name="directoryPath">The remote directory path</param>
+        void MakeDirectory(string directoryPath);
+
+        /// <summary>
+        /// Removes a directory on the remote target
+        /// </summary>
+        /// <param name="directoryPath">The remote directory path</param>
+        void RemoveDirectory(string directoryPath);
+
+        /// <summary>
+        /// Renames a file on the remote taget
+        /// </summary>
+        /// <param name="currentFilePath">The remote file path, including the file name</param>
+        /// <param name="newFileName">The new name of the file</param>
+        void Rename(string currentFilePath, string newFileName);
+
+        /// <summary>
+        /// Uploads a file to the remote target
+        /// </summary>
+        /// <param name="stream">A stream of the file</param>
+        /// <param name="filePath">The remote file path, including the file name</param>
+        void UploadFile(Stream stream, string filePath);
+    }
+}
---
+++ src/Core/Files/IFtpClientFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Files
+{
+    /// <summary>
+    /// Creates <see cref="IFtpClient"/> instances, using the default (when no name is specified) or the database instance by name
+    /// </summary>
+    public interface IFtpClientFactory
+    {
+        /// <summary>
+        /// Creates a new IFtpClient instance
+        /// </summary>
+        /// <param name="connectionName">The name of the connection string in the IConnectionStringProvider</param>
+        /// <returns>A new IFtpClient instance</returns>
+        IFtpClient Create(string connectionName);
+    }
+}
