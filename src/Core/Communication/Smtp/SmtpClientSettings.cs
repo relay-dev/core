ec859c6535b0d2c1414c3a70355fe8d8488f1f36
Scott Ferguson <scott.ferguson82@gmail.com>
2020-03-04 22:23:15 -0600

    Moving connection string parsing into the objects

--- src/Core/Communication/Smtp/SmtpClientSettings.cs
+++ src/Core/Communication/Smtp/SmtpClientSettings.cs
@@ -1,10 +1,31 @@
-﻿namespace Core.Communication.Smtp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Communication.Smtp
 {
     /// <summary>
     /// Settings used to connect to an SMTP server
     /// </summary>
     public class SmtpClientSettings
     {
+        /// <summary>
+        /// Initialized the immutable object
+        /// </summary>
+        /// <param name="connectionString">The connection string of the SMTP server</param>
+        public SmtpClientSettings(string connectionString)
+        {
+            Dictionary<string, string> connectionStringProperties = connectionString.Split(';')
+                .Where(prop => !string.IsNullOrEmpty(prop))
+                .Select(prop => prop.Split(new char[] { '=' }, 2))
+                .ToDictionary(prop => prop[0].Trim(), t => t[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
+
+            Host = TryGet(connectionStringProperties, "Host");
+            Port = TryGet(connectionStringProperties, "Port");
+            Username = TryGet(connectionStringProperties, "Username");
+            Password = TryGet(connectionStringProperties, "Password");
+        }
+
         /// <summary>
         /// Initialized the immutable object
         /// </summary>
@@ -23,21 +44,35 @@
         /// <summary>
         /// The host of the SMTP server
         /// </summary>
-        public string Host { get; set; }
+        public string Host { get; }
 
         /// <summary>
         /// The port of the SMTP server
         /// </summary>
-        public string Port { get; set; }
+        public string Port { get; }
 
         /// <summary>
         /// The username for the credentials of the SMTP server
         /// </summary>
-        public string Username { get; set; }
+        public string Username { get; }
 
         /// <summary>
         /// The password for the credentials of the SMTP server
         /// </summary>
-        public string Password { get; set; }
+        public string Password { get; }
+
+        #region Private
+
+        private string TryGet(Dictionary<string, string> dictionary, string key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                return dictionary[key];
+            }
+
+            return null;
+        }
+
+        #endregion
     }
 }
--- src/Core/FileHandling/FtpClientSettings.cs
+++ src/Core/FileHandling/FtpClientSettings.cs
@@ -1,4 +1,8 @@
-﻿namespace Core.FileHandling
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.FileHandling
 {
     /// <summary>
     /// Settings used to connect to an FTP drive
@@ -8,36 +12,75 @@
         /// <summary>
         /// Initialized the immutable object
         /// </summary>
+        /// <param name="connectionString">The connection string of the FTP server</param>
+        public FtpClientSettings(string connectionString)
+        {
+            Dictionary<string, string> connectionStringProperties = connectionString.Split(';')
+                .Where(prop => !string.IsNullOrEmpty(prop))
+                .Select(prop => prop.Split(new char[] { '=' }, 2))
+                .ToDictionary(prop => prop[0].Trim(), t => t[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
+
+            Host = TryGet(connectionStringProperties, "Host");
+            Port = TryGet(connectionStringProperties, "Port");
+            Username = TryGet(connectionStringProperties, "Username");
+            Password = TryGet(connectionStringProperties, "Password");
+            IsSftp = Convert.ToBoolean(TryGet(connectionStringProperties, "IsSftp"));
+        }
+
+        /// <summary>
+        /// Initialized the immutable object
+        /// </summary>
         /// <param name="host">The host of the FTP drive</param>
+        /// <param name="port">The port of the FTP drive</param>
         /// <param name="username">The username for the credentials of the FTP drive</param>
         /// <param name="password">The password for the credentials of the FTP drive</param>
         /// <param name="isSftp">Indicates is the FTP drive to connect to is SFTP</param>
-        public FtpClientSettings(string host, string username, string password, bool isSftp)
+        public FtpClientSettings(string host, string port, string username, string password, bool isSftp)
         {
             Host = host;
+            Port = port;
             Username = username;
             Password = password;
             IsSftp = isSftp;
         }
 
         /// <summary>
-        /// Indicates is the FTP drive to connect to is SFTP
-        /// </summary>
-        public bool IsSftp { get; set; }
-
-        /// <summary>
         /// The host of the FTP drive
         /// </summary>
-        public string Host { get; set; }
+        public string Host { get; }
+
+        /// <summary>
+        /// The port of the SMTP server
+        /// </summary>
+        public string Port { get; }
 
         /// <summary>
         /// The username for the credentials of the FTP drive
         /// </summary>
-        public string Username { get; set; }
+        public string Username { get; }
 
         /// <summary>
         /// The password for the credentials of the FTP drive
         /// </summary>
-        public string Password { get; set; }
+        public string Password { get; }
+
+        /// <summary>
+        /// Indicates is the FTP drive to connect to is SFTP
+        /// </summary>
+        public bool IsSftp { get; }
+
+        #region Private
+
+        private string TryGet(Dictionary<string, string> dictionary, string key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                return dictionary[key];
+            }
+
+            return null;
+        }
+
+        #endregion
     }
 }
