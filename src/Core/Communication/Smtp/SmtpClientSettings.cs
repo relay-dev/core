43ab483578eacb0d69cde971f7b32c92bc0da788
Scott Ferguson <scott.ferguson82@gmail.com>
2020-03-04 21:59:17 -0600

    Adding SmtpClientSettings

---
+++ src/Core/Communication/Smtp/SmtpClientSettings.cs
@@ -0,0 +1,43 @@
+﻿namespace Core.Communication.Smtp
+{
+    /// <summary>
+    /// Settings used to connect to an SMTP server
+    /// </summary>
+    public class SmtpClientSettings
+    {
+        /// <summary>
+        /// Initialized the immutable object
+        /// </summary>
+        /// <param name="host">The host of the SMTP server</param>
+        /// <param name="port">The port of the SMTP server</param>
+        /// <param name="username">The username for the credentials of the SMTP server</param>
+        /// <param name="password">The password for the credentials of the SMTP server</param>
+        public SmtpClientSettings(string host, string port, string username, string password)
+        {
+            Host = host;
+            Port = port;
+            Username = username;
+            Password = password;
+        }
+
+        /// <summary>
+        /// The host of the SMTP server
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// The port of the SMTP server
+        /// </summary>
+        public string Port { get; set; }
+
+        /// <summary>
+        /// The username for the credentials of the SMTP server
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password for the credentials of the SMTP server
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ src/Core/FileHandling/FtpClientSettings.cs
@@ -0,0 +1,43 @@
+﻿namespace Core.FileHandling
+{
+    /// <summary>
+    /// Settings used to connect to an FTP drive
+    /// </summary>
+    public class FtpClientSettings
+    {
+        /// <summary>
+        /// Initialized the immutable object
+        /// </summary>
+        /// <param name="host">The host of the FTP drive</param>
+        /// <param name="username">The username for the credentials of the FTP drive</param>
+        /// <param name="password">The password for the credentials of the FTP drive</param>
+        /// <param name="isSftp">Indicates is the FTP drive to connect to is SFTP</param>
+        public FtpClientSettings(string host, string username, string password, bool isSftp)
+        {
+            Host = host;
+            Username = username;
+            Password = password;
+            IsSftp = isSftp;
+        }
+
+        /// <summary>
+        /// Indicates is the FTP drive to connect to is SFTP
+        /// </summary>
+        public bool IsSftp { get; set; }
+
+        /// <summary>
+        /// The host of the FTP drive
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// The username for the credentials of the FTP drive
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password for the credentials of the FTP drive
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
