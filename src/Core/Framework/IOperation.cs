17f7c5b5fdbc70bdbd171d828e58c48e074982b0
Scott Ferguson <scott.ferguson82@gmail.com>
2020-03-11 19:39:28 -0500

    Added IOperation

---
+++ src/Core/Framework/IOperation.cs
@@ -0,0 +1,41 @@
+﻿namespace Core.Framework
+{
+    /// <summary>
+    /// Represents an operation that can be executed
+    /// </summary>
+    public interface IOperation
+    {
+        /// <summary>
+        /// Executes the operation
+        /// </summary>
+        void Execute();
+    }
+
+    /// <summary>
+    /// Represents an operation that can be executed
+    /// </summary>
+    /// <typeparam name="TResult">Output type to be returned by this operation</typeparam>
+    public interface IOperation<TResult>
+    {
+        /// <summary>
+        /// Executes the operation
+        /// </summary>
+        /// <returns>Result of the operation</returns>
+        TResult Execute();
+    }
+
+    /// <summary>
+    /// Represents an operation that can be executed
+    /// </summary>
+    /// <typeparam name="TInput">Input type to be returned by this operation</typeparam>
+    /// <typeparam name="TResult">Output type to be returned by this operation</typeparam>
+    public interface IOperation<TInput, TResult>
+    {
+        /// <summary>
+        /// Executes the operation
+        /// </summary>
+        /// <param name="input">The input to the operation</param>
+        /// <returns>Result of the operation</returns>
+        TResult Execute(TInput input);
+    }
+}
