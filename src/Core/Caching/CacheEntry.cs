324b1d9ab6f9e6f69f32fd8175397b73c9929721
Scott Ferguson <scott.ferguson82@gmail.com>
2020-03-13 18:50:43 -0500

    Adding a few fields to CacheEntry

--- src/Core/Caching/CacheEntry.cs
+++ src/Core/Caching/CacheEntry.cs
@@ -1,10 +1,12 @@
-﻿namespace Core.Caching
+﻿using System;
+
+namespace Core.Caching
 {
     /// <summary>
     /// A generic representation of an entry in a cache
     /// </summary>
     /// <typeparam name="TToCache">The type of object to cache</typeparam>
-    public class CacheEntry<TToCache>
+    internal class CacheEntry<TToCache>
     {
         /// <summary>
         /// Constructor
@@ -16,8 +18,28 @@
         }
 
         /// <summary>
+        /// The lookup key for the entry in the cache
+        /// </summary>
+        public string Key { get; }
+
+        /// <summary>
         /// The value of the cache entry
         /// </summary>
         public TToCache Value { get; }
+
+        /// <summary>
+        /// The Type of the object in the Value field
+        /// </summary>
+        public Type Type { get; }
+
+        /// <summary>
+        /// Returns <c>true</c> if the cache entry has not expired; otherwise, <c>false</c>
+        /// </summary>
+        public bool IsValid { get; private set; }
+
+        public void Invalidate()
+        {
+            IsValid = false;
+        }
     }
 }
