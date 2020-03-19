043c1d504df40d29a3d22124ce8ee0ade61f1e5e
Scott Ferguson <scott.ferguson82@gmail.com>
2020-03-19 00:25:52 -0500

    Adding IOperationFactory

---
+++ src/Core/Framework/IOperationFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Framework
+{
+    /// <summary>
+    /// Creates types that implement the IOperation interface
+    /// </summary>
+    public interface IOperationFactory : IFactory<IOperation> { }
+}
