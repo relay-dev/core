43c5d42c572c1486a6d758571432c115f54f46ce
Scott Ferguson <scott.ferguson82@gmail.com>
2020-03-24 13:53:31 -0500

    Added ValidationException

--- src/Core/Exceptions/CoreException.cs
+++ src/Core/Exceptions/CoreException.cs
@@ -4,19 +4,19 @@ using Core.Logging;
 namespace Core.Exceptions
 {
     /// <summary>
-    /// Core Exceptions a thrown only by the Core.* assemblies. There are several overloads to support concise client code.
+    /// CoreExceptions a thrown only by the Core.* assemblies. There are several overloads to support concise client code.
     /// </summary>
     public class CoreException : Exception
     {
         /// <summary>
-        /// Error Code string to be logged; Error Codes can be found in Core.Constants.ErrorCode.*
-        /// </summary>
-        public string ErrorCode { get; protected set; }
-
-        /// <summary>
         /// The Level at which an entry should be logged
         /// </summary>
         public LogLevel LogLevel { get; protected set; }
+
+        /// <summary>
+        /// Error Code string to be logged; Error Codes can be found in Core.Constants.ErrorCode.*
+        /// </summary>
+        public string ErrorCode { get; protected set; }
 
         /// <summary>
         /// Simple overload for providing a message as part of the exception
@@ -24,12 +24,12 @@ namespace Core.Exceptions
         /// <param name="message">A brief description of what the <see cref="LogEntry"/> is. This is not meant to be formatted in a specific way and is not intended to be search on. It's a human-readble descriptive note of what happened. There are other classes for providing more structure to log entries for the sake of searching and grouping</param>
         public CoreException(string message) : base(message)
         {
-            ErrorCode = Exceptions.ErrorCode.SYST;
+            ErrorCode = Exceptions.ErrorCode.CORE;
             LogLevel = LogLevel.Error;
         }
 
         /// <summary>
-        /// Simple overload for providing an <see cref="Exceptions.ErrorCode"/> as well as a message as part of the exception
+        /// Simple overload for providing an <see cref="ErrorCode"/> as well as a message as part of the exception
         /// </summary>
         /// <param name="errorCode">The Error Code string to be logged; Error Codes can be found in Core.Constants.ErrorCode.* and are 4 character strings</param>
         /// <param name="message">A brief description of the <see cref="LogEntry"/>. This is not meant to be formatted in a specific way and is not intended to be search on. It's a human-readble descriptive note of what happened. There are other classes (i.e. <see cref="LogEntry"/>) for providing more structure to log entries for the sake of searching and grouping</param>
@@ -46,7 +46,7 @@ namespace Core.Exceptions
         /// <param name="message">A brief description of what the <see cref="LogEntry"/> is. This is not meant to be formatted in a specific way and is not intended to be search on. It's a human-readble descriptive note of what happened. There are other classes for providing more structure to log entries for the sake of searching and grouping</param>
         public CoreException(Exception innerException, string message) : base(message, innerException)
         {
-            ErrorCode = Exceptions.ErrorCode.SYST;
+            ErrorCode = Exceptions.ErrorCode.CORE;
             LogLevel = LogLevel.Error;
         }
 
---
+++ src/Core/Exceptions/ValidationException.cs
@@ -0,0 +1,44 @@
+﻿using Core.Validation;
+
+namespace Core.Exceptions
+{
+    /// <summary>
+    /// Used to encapsulate and throw a validation failure as an exception
+    /// </summary>
+    public class ValidationException : CoreException
+    {
+        /// <summary>
+        /// Details about the validation failure
+        /// </summary>
+        public ValidationFailureResult ValidationFailureResult { get; protected set; }
+
+        /// <summary>
+        /// Simple overload for providing a message as part of the exception
+        /// </summary>
+        /// <param name="message">A brief description of what the <see cref="LogEntry"/> is. This is not meant to be formatted in a specific way and is not intended to be search on. It's a human-readble descriptive note of what happened. There are other classes for providing more structure to log entries for the sake of searching and grouping</param>
+        public ValidationException(string message) : base(message)
+        {
+            ErrorCode = Exceptions.ErrorCode.BADR;
+        }
+
+        /// <summary>
+        /// Simple overload for providing an <see cref="ErrorCode"/> as well as a message as part of the exception
+        /// </summary>
+        /// <param name="errorCode">The Error Code string to be logged; Error Codes can be found in Core.Constants.ErrorCode.* and are 4 character strings</param>
+        /// <param name="message">A brief description of the <see cref="LogEntry"/>. This is not meant to be formatted in a specific way and is not intended to be search on. It's a human-readble descriptive note of what happened. There are other classes (i.e. <see cref="LogEntry"/>) for providing more structure to log entries for the sake of searching and grouping</param>
+        public ValidationException(string errorCode, string message) : base(errorCode, message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        /// <summary>
+        /// Simple overload for providing a <see cref="Validation.ValidationFailureResult"/> as part of the exception
+        /// </summary>
+        /// <param name="validationFailureResult">Details about the validation failure</param>
+        public ValidationException(ValidationFailureResult validationFailureResult)
+            : base("Input validation failed")
+        {
+            ValidationFailureResult = validationFailureResult;
+        }
+    }
+}
