e7bafbb6f5bb254c4d5b9490310f31f38dfaf0db
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-22 19:43:36 -0500

    Adding IConnectionStringProvider.GetOptional(string connectionName)

--- src/Core/Providers/IConnectionStringProvider.cs
+++ src/Core/Providers/IConnectionStringProvider.cs
@@ -8,10 +8,17 @@ namespace Core.Providers
     public interface IConnectionStringProvider : IProvider<string>
     {
         /// <summary>
-        /// Gets a connection string by the connection name
+        /// Gets a connection string by the connection name and throws an exception if the connection string is not found
         /// </summary>
         /// <param name="connectionName">The name of the connection string in the configuration</param>
         /// <returns>The connection string</returns>
         string Get(string connectionName);
+
+        /// <summary>
+        /// Gets a connection string by the connection name and returns null if the connection string is not found
+        /// </summary>
+        /// <param name="connectionName">The name of the connection string in the configuration</param>
+        /// <returns>The connection string</returns>
+        string GetOptional(string connectionName);
     }
 }
