99f2822c4d9a3665e5d58cbdd07343f86f5f966d
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-23 19:13:54 -0500

    Adding IWrite

---
+++ src/Core/Framework/IWrite.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Framework
+{
+    /// <summary>
+    /// Represents an object sent by a client with the intent to either insert or update data
+    /// </summary>
+    public interface IWrite
+    {
+        /// <summary>
+        /// The username that should be stamped on the CreatedBy or ModifiedBy
+        /// </summary>
+        public string Username { get; set; }
+    }
+}
