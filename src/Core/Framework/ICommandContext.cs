bfb3bc14a9a8520ca0d0b216eeec1ab8e5a224ab
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-23 22:00:08 -0500

    Adding ICommandContext

---
+++ src/Core/Framework/ICommandContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Framework
+{
+    /// <summary>
+    /// Provides information about the current command being executed
+    /// </summary>
+    public interface ICommandContext
+    {
+        /// <summary>
+        /// The Username from the command request to be used when auditing entities
+        /// </summary>
+        string Username { get; set; }
+    }
+}
