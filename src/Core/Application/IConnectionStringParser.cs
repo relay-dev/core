4cb92ff99c8418afc91298de779717a61002c16d
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-23 19:52:12 -0500

    Adding IConnectionStringParser

---
+++ src/Core/Application/IConnectionStringParser.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Application
+{
+    /// <summary>
+    /// Parses a connection string into segments
+    /// </summary>
+    public interface IConnectionStringParser
+    {
+        /// <summary>
+        /// The value of the segment, if the segment key is present
+        /// </summary>
+        dynamic Segment { get; }
+
+        /// <summary>
+        /// Attempts to get a segment value by key
+        /// </summary>
+        /// <param name="key">The key of the segment</param>
+        /// <returns>The value of the segment, if the segment key is present</returns>
+        string TryGet(string key);
+    }
+}
