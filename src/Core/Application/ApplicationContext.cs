a56e0474619e9202c64bdc38e0e72f441db2462f
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 16:46:01 -0500

    Simplifying the BuildTimestamp

--- src/Core/Application/ApplicationContext.cs
+++ src/Core/Application/ApplicationContext.cs
@@ -36,7 +36,7 @@ namespace Core.Application
             ApplicationName = applicationName;
             ApplicationId = applicationId;
             ApplicationVersion = applicationVersion;
-            BuildTimestamp = GetBuildTimestamp(typeFromAssembly);
+            BuildTimestamp = File.GetLastWriteTime(typeFromAssembly.Assembly.Location);
         }
 
         /// <summary>
@@ -58,24 +58,5 @@ namespace Core.Application
         /// The timestamp at which this application was built
         /// </summary>
         public DateTime BuildTimestamp { get; }
-
-        private static DateTime GetBuildTimestamp(Type type)
-        {
-            const int peHeaderOffset = 60;
-            const int linkerTimestampOffset = 8;
-            var bytes = new byte[2048];
-
-            using (var file = new FileStream(type.Assembly.Location, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                file.Read(bytes, 0, bytes.Length);
-            }
-
-            var headerPos = BitConverter.ToInt32(bytes, peHeaderOffset);
-            var secondsSince1970 = BitConverter.ToInt32(bytes, headerPos + linkerTimestampOffset);
-            var dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            var linkTimeUtc = dt.AddSeconds(secondsSince1970);
-
-            return TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, TimeZoneInfo.Local);
-        }
     }
 }
