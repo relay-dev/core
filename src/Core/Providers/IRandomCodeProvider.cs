98f175815e8c8da7e50d16ec3f3837d7b4d61836
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 01:22:05 -0500

    Adding interfaces for providers

---
+++ src/Core/Providers/IRandomCodeProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Providers
+{
+    /// <summary>
+    /// Generates a random alpha-numeric code
+    /// </summary>
+    public interface IRandomCodeProvider
+    {
+        /// <summary>
+        /// Generates a random alpha-numeric code
+        /// </summary>
+        /// <param name="length">The length of the code to generate</param>
+        /// <param name="prefix">The prefix to be used on the code</param>
+        /// <param name="suffix">The suffix to be used on the code</param>
+        /// <returns>The random alpha-numeric code</returns>
+        string Get(int length, string prefix = null, string suffix = null);
+    }
+}
--- src/Core/Providers/IRandomLongProvider.cs
+++ src/Core/Providers/IRandomLongProvider.cs
@@ -5,5 +5,14 @@ namespace Core.Providers
     /// <summary>
     /// Provides Commands with a random long
     /// </summary>
-    public interface IRandomLongProvider : IProvider<long> { }
+    public interface IRandomLongProvider : IProvider<long>
+    {
+        /// <summary>
+        /// Provides Commands with a random long
+        /// </summary>
+        /// <param name="minValue">The lower limit of the number</param>
+        /// <param name="maxValue">The upper limit of the number</param>
+        /// <returns></returns>
+        long Get(long minValue, long maxValue);
+    }
 }
