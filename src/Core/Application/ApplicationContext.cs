8e27c6b456fceb2b4f22ba07eda6ef5de97ac28c
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 16:28:41 -0500

    Adding some functionality to determine the build timestamp from a type that exists in the client code

--- src/Core/Application/ApplicationContext.cs
+++ src/Core/Application/ApplicationContext.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Framework;
 using System;
+using System.IO;
 
 namespace Core.Application
 {
@@ -24,6 +25,21 @@ namespace Core.Application
         }
 
         /// <summary>
+        /// Constructor
+        /// </summary>
+        public ApplicationContext(
+            string applicationName,
+            Type typeFromAssembly,
+            long applicationId = 0,
+            string applicationVersion = "")
+        {
+            ApplicationName = applicationName;
+            ApplicationId = applicationId;
+            ApplicationVersion = applicationVersion;
+            BuildTimestamp = GetBuildTimestamp(typeFromAssembly);
+        }
+
+        /// <summary>
         /// The unique Id of the application
         /// </summary>
         public long ApplicationId { get; }
@@ -42,5 +58,24 @@ namespace Core.Application
         /// The timestamp at which this application was built
         /// </summary>
         public DateTime BuildTimestamp { get; }
+
+        private static DateTime GetBuildTimestamp(Type type)
+        {
+            const int peHeaderOffset = 60;
+            const int linkerTimestampOffset = 8;
+            var bytes = new byte[2048];
+
+            using (var file = new FileStream(type.Assembly.Location, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                file.Read(bytes, 0, bytes.Length);
+            }
+
+            var headerPos = BitConverter.ToInt32(bytes, peHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(bytes, headerPos + linkerTimestampOffset);
+            var dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var linkTimeUtc = dt.AddSeconds(secondsSince1970);
+
+            return TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, TimeZoneInfo.Local);
+        }
     }
 }
