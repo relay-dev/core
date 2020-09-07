aac1803d71da80f5b20c6cea5ee44950194a6ccc
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 15:27:40 -0500

    Adding PaginationRequest

---
+++ src/Core/Framework/IPaginate.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Framework
+{
+    /// <summary>
+    /// Represents a request that contains pagination instructions 
+    /// </summary>
+    public interface IPaginate
+    {
+        /// <summary>
+        /// The number of records to skip
+        /// </summary>
+        public int? Skip { get; set; }
+
+        /// <summary>
+        /// The number of records to take
+        /// </summary>
+        public int? Take { get; set; }
+    }
+}
---
+++ src/Core/Framework/PaginationRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Framework
+{
+    /// <summary>
+    /// A request that contains pagination instructions
+    /// </summary>
+    public class PaginationRequest : IPaginate
+    {
+        /// <summary>
+        /// The number of records to skip
+        /// </summary>
+        public int? Skip { get; set; }
+
+        /// <summary>
+        /// The number of records to take
+        /// </summary>
+        public int? Take { get; set; }
+    }
+}
