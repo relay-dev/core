55d847ef082f4cbbb00bc6d6c8f228b4ce346dfe
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 00:58:59 -0500

    Adding overload to CommandContext

--- src/Core/Framework/CommandContext.cs
+++ src/Core/Framework/CommandContext.cs
@@ -6,6 +6,20 @@
     public class CommandContext
     {
         /// <summary>
+        /// Constructor
+        /// </summary>
+        public CommandContext() { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="username"></param>
+        public CommandContext(string username)
+        {
+            Username = username;
+        }
+
+        /// <summary>
         /// The Username from the command request to be used when auditing entities
         /// </summary>
         public string Username { get; set; }
