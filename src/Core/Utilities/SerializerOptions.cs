803e6d8b143989e92a68b408eab58794a5aabcbd
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-11 21:06:58 -0500

    Adding a few types that could be generalized

---
+++ src/Core/Utilities/SerializerOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Utilities
+{
+    /// <summary>
+    /// A generic set of options for a serializer to use
+    /// </summary>
+    public class SerializerOptions
+    {
+        public bool AllowTrailingCommas { get; set; }
+        public int DefaultBufferSize { get; set; }
+        public string Encoder { get; set; }
+        public string DictionaryKeyPolicy { get; set; }
+        public bool IgnoreNullValues { get; set; }
+        public bool IgnoreReadOnlyProperties { get; set; }
+        public int MaxDepth { get; set; }
+        public string PropertyNamingPolicy { get; set; }
+        public bool PropertyNameCaseInsensitive { get; set; }
+        public string ReadCommentHandling { get; set; }
+        public bool WriteIndented { get; set; }
+    }
+}
