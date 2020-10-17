8d5f9bffee4aabc66c2e1d733911e18f3be4857a
Scott Ferguson <scott.ferguson82@gmail.com>
2020-10-16 23:56:35 -0500

    Adding a setter to the IUsernameProvider

--- src/Core/Providers/IUsernameProvider.cs
+++ src/Core/Providers/IUsernameProvider.cs
@@ -5,5 +5,12 @@ namespace Core.Providers
     /// <summary>
     /// Provides Commands with the username which is relevant under the current context
     /// </summary>
-    public interface IUsernameProvider : IProvider<string> { }
+    public interface IUsernameProvider : IProvider<string>
+    {
+        /// <summary>
+        /// Sets the username field
+        /// </summary>
+        /// <param name="username">The username to encapsulate</param>
+        void Set(string username);
+    }
 }
