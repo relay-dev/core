b039c8c123aab22a4997371dc364ae0d79d0af5e
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-17 18:37:47 -0600

    Adding to the Processor types

--- src/Core/Framework/IProcessor.cs
+++ src/Core/Framework/IProcessor.cs
@@ -11,6 +11,37 @@ namespace Core.Framework
         /// <summary>
         /// Invokes the process
         /// </summary>
-        Task ProcessAsync(CancellationToken cancellationToken = default);
+        /// <param name="cancellationToken">The current cancellation token</param>
+        Task ProcessAsync(CancellationToken cancellationToken);
+    }
+
+    /// <summary>
+    /// Represents a simple class that runs a process
+    /// </summary>
+    /// <typeparam name="TInput">The input to the processor</typeparam>
+    public interface IProcessor<TInput>
+    {
+        /// <summary>
+        /// Invokes the process
+        /// </summary>
+        /// <param name="input">The input to the processor</param>
+        /// <param name="cancellationToken">The current cancellation token</param>
+        Task ProcessAsync(TInput input, CancellationToken cancellationToken);
+    }
+
+    /// <summary>
+    /// Represents a simple class that runs a process
+    /// </summary>
+    /// <typeparam name="TInput">The input to the processor</typeparam>
+    /// <typeparam name="TOutput">The output of the processor</typeparam>
+    public interface IProcessor<TInput, TOutput>
+    {
+        /// <summary>
+        /// Invokes the process
+        /// </summary>
+        /// <param name="input">The input to the processor</param>
+        /// <param name="cancellationToken">The current cancellation token</param>
+        /// <returns>The output of the processor</returns>
+        Task<TOutput> ProcessAsync(TInput input, CancellationToken cancellationToken);
     }
 }
--- src/Core/Framework/ProcessorAttribute.cs
+++ src/Core/Framework/ProcessorAttribute.cs
@@ -5,12 +5,17 @@ namespace Core.Framework
     /// <summary>
     /// Represents a simple class that runs a process
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class)]
     public sealed class ProcessorAttribute : Attribute
     {
         /// <summary>
         /// Specifies the name of the process the class represents
         /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Specifies the category of the process
+        /// </summary>
+        public string Category { get; set; }
     }
 }
