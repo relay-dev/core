e62c39e6573aab7fb01443eedf069faea1b7fd9e
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-22 15:47:22 -0600

    Adding overloads to the assemblyScanner

--- src/Core/Utilities/IAssemblyScanner.cs
+++ src/Core/Utilities/IAssemblyScanner.cs
@@ -13,29 +13,56 @@ namespace Core.Utilities
         /// Scans assemblies and returns a collection of types that utilize a given attribute type and match an optional predicate argument if specified
         /// </summary>
         /// <typeparam name="TAttribute">The attribute type to search all class in the assemblies for</typeparam>
-        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
         /// <param name="assemblies">The assemblies to scan</param>
-        /// <returns>All Core types that utilize the given attribute type and match the predicate argument</returns>
+        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <returns>All types that utilize the given attribute type and match the predicate argument</returns>
         IEnumerable<Type> FindTypesWithAttribute<TAttribute>(IEnumerable<Assembly> assemblies, Func<TAttribute, bool> predicate = null) where TAttribute : Attribute;
 
         /// <summary>
         /// Scans assemblies and returns a collection of types that utilize a given attribute type and match an optional predicate argument if specified
         /// </summary>
-        /// <typeparam name="TBaseClass">The base class to search all class in the assemblies for</typeparam>
-        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <param name="type">The attribute type to search all class in the assemblies for</param>
         /// <param name="assemblies">The assemblies to scan</param>
-        /// <returns>All Core types that utilize the given attribute type and match the predicate argument</returns>
+        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <returns>All types that utilize the given attribute type and match the predicate argument</returns>
+        IEnumerable<Type> FindTypesWithAttribute(Type type, IEnumerable<Assembly> assemblies, Func<object, bool> predicate = null);
+
+        /// <summary>
+        /// Scans assemblies and returns a collection of types that utilize a given attribute type and match an optional predicate argument if specified
+        /// </summary>
+        /// <typeparam name="TBaseClass">The base class to search all class in the assemblies for</typeparam>
+        /// <param name="assemblies">The assemblies to scan</param>
+        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <returns>All types that utilize the given attribute type and match the predicate argument</returns>
         IEnumerable<Type> FindTypesWithBaseClass<TBaseClass>(IEnumerable<Assembly> assemblies, Func<Type, bool> predicate = null);
 
         /// <summary>
         /// Scans assemblies and returns a collection of types that utilize a given attribute type and match an optional predicate argument if specified
         /// </summary>
-        /// <typeparam name="TInterface">The interface to search all class in the assemblies for</typeparam>
-        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <param name="type">The base class to search all class in the assemblies for</param>
         /// <param name="assemblies">The assemblies to scan</param>
-        /// <returns>All Core types that utilize the given attribute type and match the predicate argument</returns>
+        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <returns>All types that utilize the given attribute type and match the predicate argument</returns>
+        IEnumerable<Type> FindTypesWithBaseClass(Type type, IEnumerable<Assembly> assemblies, Func<Type, bool> predicate = null);
+
+        /// <summary>
+        /// Scans assemblies and returns a collection of types that utilize a given attribute type and match an optional predicate argument if specified
+        /// </summary>
+        /// <typeparam name="TInterface">The interface to search all class in the assemblies for</typeparam>
+        /// <param name="assemblies">The assemblies to scan</param>
+        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <returns>All types that utilize the given attribute type and match the predicate argument</returns>
         IEnumerable<Type> FindTypesWithInterface<TInterface>(IEnumerable<Assembly> assemblies, Func<Type, bool> predicate = null);
-        
+
+        /// <summary>
+        /// Scans assemblies and returns a collection of types that utilize a given attribute type and match an optional predicate argument if specified
+        /// </summary>
+        /// <param name="type">The interface to search all class in the assemblies for</param>
+        /// <param name="assemblies">The assemblies to scan</param>
+        /// <param name="predicate">Optional; the condition of the types to match in order to include them in the return collection</param>
+        /// <returns>All types that utilize the given attribute type and match the predicate argument</returns>
+        IEnumerable<Type> FindTypesWithInterface(Type type, IEnumerable<Assembly> assemblies, Func<Type, bool> predicate = null);
+
         /// <summary>
         /// Gets all assemblies within the scope of the application
         /// </summary>
