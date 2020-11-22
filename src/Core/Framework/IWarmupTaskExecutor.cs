576d327864be518001b8f770f717fd42b58f34b9
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-22 11:35:18 -0600

    Adding IWarmupTaskExecutor

---
+++ src/Core/Framework/IWarmupTaskExecutor.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Framework
+{
+    /// <summary>
+    /// Runs any configured warmup tasks
+    /// </summary>
+    public interface IWarmupTaskExecutor
+    {
+        /// <summary>
+        /// Runs the warmup tasks
+        /// </summary>
+        /// <param name="cancellationToken">The current <see cref="CancellationToken"/></param>
+        Task RunAsync(CancellationToken cancellationToken);
+    }
+}
